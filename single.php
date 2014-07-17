e89fdd638a36a493210c37db55adc3b1504cdcbf
Stephen Parente <webmakersteve@gmail.com>
2014-07-17 00:05:08 -0400

    Got rid of css files made them PHP

--- single.php
+++ single.php
@@ -1,8 +1,34 @@
-/*
-Theme Name: Wordpress Bootstrap
-Author: Ingenuity Design
-Description: Theme to install bootstrap initializr on. Bare bones
-Version: 1.0
+<?php get_header(); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="jumbotron">
+        <div class="container">
+            <h1><?php the_title() ?></h1>
+            <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
+            <!--p><a class="btn btn-inverse btn-lg" role="button">Learn more &raquo;</a></p-->
+        </div>
+    </div>
 
-*/
+    <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
 
+                <div class="col-md-6 col-md-offset-3">
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
+                    <?php the_content(); ?>
+
+                </div>
+
+
+        </div>
+
+        <hr>
+
+    </div>
+
+<?php endwhile; else: ?>
+    <div class="col-md-6 col-md-offset-3">
+        No Posts
+    </div>
+<?php endif; ?>
+
+<?php get_footer(); ?>
--- tag.php
+++ tag.php
@@ -1,8 +1,31 @@
-/*
-Theme Name: Wordpress Bootstrap
-Author: Ingenuity Design
-Description: Theme to install bootstrap initializr on. Bare bones
-Version: 1.0
+<?php get_header(); ?>
 
-*/
+    <div class="jumbotron">
+        <div class="container">
+            <h1><php the_title(); ?></h1>
 
+            <!--p><a class="btn btn-inverse btn-lg" role="button">Learn more &raquo;</a></p-->
+        </div>
+    </div>
+
+    <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <div class="col-md-6 col-md-offset-3">
+                    <?php the_content(); ?>
+                    <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
+                </div>
+            <?php endwhile; else: ?>
+                <div class="col-md-6 col-md-offset-3">
+                    No Posts
+                </div>
+            <?php endif; ?>
+
+        </div>
+
+        <hr>
+
+    </div>
+
+<?php get_footer(); ?>
