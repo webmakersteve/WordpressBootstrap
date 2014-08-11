c96fb68e8304ecb61a15da62d54d9cb055fd07ad
Stephen Parente <stephen@ingenuitydesign.com>
2014-08-11 14:56:54 -0400

    Added wp_title and got rid of description

--- functions.php
+++ functions.php
@@ -73,3 +73,37 @@ function bootstrap_nav_menu( $overrides = null ) {
 
     wp_nav_menu( $defaults );
 }
+
+/**
+ * Filter the page title.
+ *
+ * Creates a nicely formatted and more specific title element text for output
+ * in head of document, based on current view.
+ *
+ * @since Twenty Thirteen 1.0
+ *
+ * @param string $title Default title text for current view.
+ * @param string $sep   Optional separator.
+ * @return string The filtered title.
+ */
+function bootstrap_wp_title( $title, $sep ) {
+	global $paged, $page;
+
+	if ( is_feed() )
+		return $title;
+
+	// Add the site name.
+	$title .= get_bloginfo( 'name', 'display' );
+
+	// Add the site description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title = "$title $sep $site_description";
+
+	// Add a page number if necessary.
+	if ( $paged >= 2 || $page >= 2 )
+		$title = "$title $sep " . sprintf( __( 'Page %s', 'twentythirteen' ), max( $paged, $page ) );
+
+	return $title;
+}
+add_filter( 'wp_title', 'bootstrap_wp_title', 10, 2 );
